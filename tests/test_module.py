d0d075734e13269a8740a5261ddeb6aecdbb4ea6
jgstew <james@jgstew.com>
2021-08-22 13:22:35 -0400

    fixing pylint errors and other issues

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,3 +1,9 @@
+"""
+This python module defines tests for pytest
+
+run `pytest -v` at the root to get results
+"""
+
 import os.path
 import sys
 
@@ -19,23 +25,24 @@ import validate_bes_xml  # pylint: disable=import-error,wrong-import-position
 
 
 def test_module_location():
-    # check for only 'src' so it will work on windows and non-windows
+    """check for only 'src' so it will work on windows and non-windows"""
     assert "src" in validate_bes_xml.__file__
 
 
 def test_schemas_found():
-    # must be 4 valid schema files:
+    """must be 4 valid schema files:"""
     assert len(validate_bes_xml.validate_bes_xml.find_schema_files()) == 4
 
 
 def test_examples():
-    # should be 2 errors currently:
+    """should be EXPECTED_ERROR_COUNT errors currently:"""
     assert (
         validate_bes_xml.validate_bes_xml.validate_all_files() == EXPECTED_ERROR_COUNT
     )
 
 
 def test_main():
+    """test main exit code = EXPECTED_ERROR_COUNT"""
     with pytest.raises(SystemExit) as wrapped_error:
         validate_bes_xml.validate_bes_xml.main()
     # https://medium.com/python-pandemonium/testing-sys-exit-with-pytest-10c6e5f7726f
@@ -43,23 +50,26 @@ def test_main():
 
 
 def test_validate_xml():
+    """test single xml files"""
     assert (
         validate_bes_xml.validate_bes_xml.validate_xml(
             "tests/examples/good/FixletDebugger.bes"
         )
-        == True  # noqa
+        is True
     )
     assert (
         validate_bes_xml.validate_bes_xml.validate_xml(
             "tests/examples/bad/example_bes.bes"
         )
-        == False  # noqa
+        is False
     )
 
 
 def test_isort():
+    """test isort results"""
     assert ret_code_isort == 0
 
 
 def test_flakes():
+    """test pyflakes results"""
     assert ret_code_flakes == 0
